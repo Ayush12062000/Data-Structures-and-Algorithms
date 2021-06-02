9bcc08fe666a9103354b24f0fd265faba91d5071
Ayush12062000 <kaayush112@gmail.com>
2021-06-02 21:11:11 +0530

    Cycle in Graph

--- Graphs/Cycle_in_Graph.cpp
+++ Graphs/Cycle_in_Graph.cpp
@@ -2,11 +2,43 @@
 #define input(n) int n; cin>>n
 using namespace std;
 
+void dfs(vector<int> g[],int i,int v,int visited[])
+{
+
+}
 
 
 int main()
 {
+    cout<<"Enter vertices: ";
+    input(v);
+    cout<<"Enter Edges: ";
+    input(e);
+    vector<int> g[v];
+    int flag = 0;
+    int visited[v];
+    for(int i=0;i<v;i++)
+        visited[i] = 0;
 
+    for(int i=0;i<e;i++)
+    {
+        input(s);
+        input(d);
+        g[s].push_back(d);
+        g[d].push_back(s);
+    }
+
+    for(int i=0;i<v;i++)
+    {
+        if(!visited[i])
+            dfs(g,i,v,visited);
+        if(flag==1)
+            break;
+    }
+    if(flag)
+        cout<<"Cycle exist\n";
+    else
+        cout<<"No Cycle exist\n";
     return 0;
 }
 
